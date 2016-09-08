05533bf011966380eab4e28d3ab01c67a7986a31
Anna Dee <freetowninred@gmail.com>
2016-09-08 23:15:00 +0300

    added 2.part of homework js-7-8

---
+++ js-7-8_2/js/script.js
@@ -0,0 +1,52 @@
+'use strict';
+$(document).ready(function () {
+
+var $button = $('button');
+
+var $tips = $('.tips');
+var $tip1 = $('.tip-1');
+var $tip2 = $('.tip-2');
+var $tip3 = $('.tip-3');
+
+var $label1 = $('.label-1');
+var $label2 = $('.label-2');
+var $label3 = $('.label-3');
+
+// function getHelp() {
+//   $tips.show();
+//   // $tips.css('display', 'block');
+// };
+
+$button.on('click', function () {
+  $tips.css('display', 'block');
+});
+
+$label1.hover(function () {
+  $tip1.css('display', 'block');},
+function () {
+  $tip1.css('display', 'none');}
+);
+
+$label2.hover(function () {
+  $tip2.css('display', 'block');},
+function () {
+  $tip2.css('display', 'none');}
+);
+
+$label3.hover(function () {
+  $tip3.css('display', 'block');},
+function () {
+  $tip3.css('display', 'none');}
+);
+
+
+
+
+
+
+
+
+
+
+
+})
