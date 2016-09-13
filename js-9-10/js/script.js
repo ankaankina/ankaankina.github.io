cf1f6311433b155c0520afbf5114a782d3083262
Anna Dee <freetowninred@gmail.com>
2016-09-13 21:57:58 +0300

    first stage of homework js 9-10

---
+++ js-9-10/js/script.js
@@ -0,0 +1,44 @@
+'use strict'
+
+$(document).ready(function () {
+
+  // DROP DOWN MENU
+  var dropDownMenuItem = $('.menu-item-dropdown');
+  var dropDownSubMenuItem = $('.sub-menu-link-dropdown');
+
+  dropDownMenuItem.hover(function(){
+      $(this).children('.sub-menu').slideDown(200);
+    }, function(){
+      $(this).children('.sub-menu').slideUp(200);
+    })
+
+
+  // SLIDER
+  $(".fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+
+  // $(function() {
+  //   $(".jcarousel").jcarousel({
+  //     list: '.jcarousel-list',
+  //     rtl: true
+  //   });
+  // });
+
+  var $buttonLinks = $('.jcarousel-btn');
+  $buttonLinks.on('click', function (e) {
+    e.preventDefault();
+  })
+
+  $('.jcarousel-prev').click(function() {
+    $('.jcarousel').jcarousel('scroll', '-=1');
+  });
+
+  $('.jcarousel-next').click(function() {
+    $('.jcarousel').jcarousel('scroll', '+=1');
+  });
+
+
+
+})
