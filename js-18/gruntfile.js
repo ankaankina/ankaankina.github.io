2cb8ac4262293798dcf7cd1868840631f91b2763
Anna Dee <freetowninred@gmail.com>
2016-10-05 23:51:52 +0300

    finished js-18 with grunt

--- js-18/gruntfile.js
+++ js-18/gruntfile.js
@@ -10,23 +10,28 @@ module.exports = function(grunt) {
         dest: 'js/build/main.js'
       }
     },
-    uglify: [{
+    uglify: {
       dist: {
         src: ['js/build/main.js'],
         dest: 'js/build/main.min.js'
       }
     },
-    {
-      dist: {
-        src: ['css/style.css'],
-        dest: 'css/style.min.css'
+    cssmin: {
+      options: {
+        banner: '/*\n css/*.css <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'css/style.min.css': 'css/style.css'
+        }
       }
-    }]
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 
 };
