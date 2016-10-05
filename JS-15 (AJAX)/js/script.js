f7ef5f1b21de12684f492f09c5d6fe1b5daf6a72
Anna Dee <freetowninred@gmail.com>
2016-10-05 13:30:43 +0300

    m

--- JS-15 (AJAX)/js/script.js
+++ JS-15 (AJAX)/js/script.js
@@ -1,26 +1,24 @@
 function GoogleCallback (jQueryObj, data) {
-    console.log('data', data);
+    console.log("data", data);
 }
 
 
 $(function() {
 
     $.ajax({
-        url: 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q='+'test'+'&callback=GoogleCallback&context=?',
+        url: "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=AIzaSyBoHXZGOIX6QXe6ao5VnOEE5Z_hgQg0RC4&rsz=large&q="+"test"+"&callback=GoogleCallback&context=?",
         data: {
           abc: 123
         },
         dataType: "jsonp",
-        method: 'POST',
+        method: "POST",
         success: function () {
-          console.log(':)');
+          console.log(":)");
         },
         error: function () {
-          console.log(':(');
+          console.log(":(");
         }
     });
-
-
 
 
 
