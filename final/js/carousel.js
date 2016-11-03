b8b8b9b85b0eb8696bd514f5ef1e76f379389956
Anna Dee <freetowninred@gmail.com>
2016-11-03 13:45:18 +0200

    added slider but it's not working

---
+++ final/js/carousel.js
@@ -0,0 +1,30 @@
+$(document).ready(slider);
+
+function slider() {
+  var leftUIEl = $('.arrowl-1');
+  var rightUIEl = $('.arrowr-1');
+  var elementsList = $('#slider_1');
+
+  var pixelOffset = 600;
+  var currentLeftValue = 0;
+  var elementsCount = elementsList.find('.slide').length;
+  var minimumOffset = -((elementsCount - 5) * pixelOffset);
+  var maximumOffset = 0;
+
+  leftUIEl.on('click', slideLeft);
+  rightUIEl.on('click', slideRight);
+
+  function slideLeft() {
+    if (currentLeftValue != maximumOffset) {
+      currentLeftValue += 300;
+      elementsList.animate({left: currentLeftValue + 'px'}, 500);
+    };
+  };
+
+  function slideRight() {
+    if (currentLeftValue != minimumOffset) {
+      currentLeftValue -= 300;
+      elementsList.animate({left: currentLeftValue + 'px'}, 800);
+    };
+  };
+}
